a2fcbee0820a44751423cc2f218640cfa352e2b1
vasu081992 <vasudevan29.92@gmail.com>
2024-04-27 11:13:55 +0530

    commit latest apr27

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+//import Card1 from './components/Card/Card';
+import React from "react"
 
 const App = () => {
   return (
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Card.module.css"
+import Card from '@mui/material/Card';
+
+
+ Card1 = () =>{
+
+return(
+
+<div className={styles.card}>
+
+
+</div>
+
+
+)
+
+}
+
+export default Card1
